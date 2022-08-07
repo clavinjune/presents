64a32fb84df6928eb25b2360b23600d639e2860a
Clavianus Juneardo <24659468+clavinjune@users.noreply.github.com>
2022-08-07 17:34:58 +0700

    update content
    Signed-off-by: Clavianus Juneardo <24659468+clavinjune@users.noreply.github.com>

--- developing-api-services-with-golang-at-finditgeek/assets/fetch-all-tasks-test.go
+++ developing-api-services-with-golang-at-finditgeek/assets/fetch-all-tasks-test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	resp, _ := http.Get("http://localhost:8080/v1/tasks")
+	resp, _ := http.Get("http://localhost:8000/v1/tasks")
 	defer resp.Body.Close()
 	b, _ := io.ReadAll(resp.Body)
 	fmt.Println(string(b))
