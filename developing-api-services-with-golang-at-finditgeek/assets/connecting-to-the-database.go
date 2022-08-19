9a6ad32b0417dc87cb1a498b47f43f5fc0813d8d
Clavianus Juneardo <24659468+clavinjune@users.noreply.github.com>
2022-08-19 16:12:23 +0700

    fix typo
    Signed-off-by: Clavianus Juneardo <24659468+clavinjune@users.noreply.github.com>

--- developing-api-services-with-golang-at-finditgeek/assets/connecting-to-the-database.go
+++ developing-api-services-with-golang-at-finditgeek/assets/connecting-to-the-database.go
@@ -1,10 +1,11 @@
 const (
 	queryMigrate string = `CREATE TABLE IF NOT EXISTS tasks(
-		id TEXT PRIMARY KEY,
-		name TEXT NOT NULL UNIQUE
-	)`
+        id TEXT PRIMARY KEY,
+        name TEXT NOT NULL UNIQUE,
+        is_active BOOLEAN NOT NULL DEFAULT TRUE
+    )`
 
-	querySeed string = `INSERT INTO tasks VALUES('seed1', 'task seed 1')`
+	querySeed string = `INSERT INTO tasks (id, name) VALUES('seed1', 'task seed 1')`
 )
 
 // this function is simplified // HL1
