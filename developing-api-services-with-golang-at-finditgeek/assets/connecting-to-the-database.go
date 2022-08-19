c20eaee7d685284a4eef27eb504aedd99c556f79
Clavianus Juneardo <24659468+clavinjune@users.noreply.github.com>
2022-08-19 14:21:51 +0700

    add slide
    Signed-off-by: Clavianus Juneardo <24659468+clavinjune@users.noreply.github.com>

---
+++ developing-api-services-with-golang-at-finditgeek/assets/connecting-to-the-database.go
@@ -0,0 +1,17 @@
+const (
+	queryMigrate string = `CREATE TABLE IF NOT EXISTS tasks(
+		id TEXT PRIMARY KEY,
+		name TEXT NOT NULL UNIQUE
+	)`
+
+	querySeed string = `INERT INTO tasks VALUES('seed1', 'task seed 1')`
+)
+
+// this function is simplified // HL1
+// please don't use this function in production // HL1
+func initializeDatabase() *sql.DB {
+	sql.Open("sqlite3", ":memory:")
+	db.Exec(queryMigrate)
+	db.Exec(querySeed)
+	return db
+}
